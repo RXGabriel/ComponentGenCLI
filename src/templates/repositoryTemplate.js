f256822e4f90b3db8b73f22108d662073f8b4998
RXGabriel <gabriel2005rf@gmail.com>
2024-04-11 10:12:49 -0300

    feat: Add initial project structure

---
+++ src/templates/repositoryTemplate.js
@@ -0,0 +1,21 @@
+const template = `
+export default class RepositoryTemplate {
+    constructor() { }
+    create(data) {
+        return Promise.reject("method not implemented!")
+    }
+    read(query) {
+        return Promise.reject("method not implemented!")
+    }
+    update(id, data) {
+        return Promise.reject("method not implemented!")
+    }
+
+    delete(id) {
+        return Promise.reject("method not implemented!")
+    }
+}`
+
+export function repositoryTemplate(componentName) {
+
+}
