239ee079757d9006d66294492d68e1f306223ac2
RXGabriel <gabriel2005rf@gmail.com>
2024-04-15 10:45:27 -0300

    test: Ensure creation of layers if they don't exist

---
+++ test/unit/layers.test.js
@@ -0,0 +1,22 @@
+import { expect, describe, test, jest, beforeEach } from '@jest/globals'
+import fsPromises from 'fs/promises'
+import fs from 'fs'
+import { createLayersIfNotExists } from '../../src/createlayers.js'
+
+describe('#Layers - Folder Structure', () => {
+    const defaultLayers = ['service', 'factory', 'repository']
+    beforeEach(() => {
+        jest.restoreAllMocks()
+        jest.clearAllMocks()
+    })
+    test('should create folders if it does\n exists', async () => {
+        jest.spyOn(fsPromises, fsPromises.mkdir.name).mockResolvedValue()
+        jest.spyOn(fs, fs.existsSync.name).mockReturnValue(false)
+
+        await createLayersIfNotExists({ mainPath: '', layers: defaultLayers })
+
+        expect(fs.existsSync).toHaveBeenCalledTimes(defaultLayers.length)
+        expect(fsPromises.mkdir).toHaveBeenCalledTimes(defaultLayers.length)
+    })
+
+})
