4b307b370003e9dc52aed56a0cd0e4445f7b2db3
RXGabriel <gabriel2005rf@gmail.com>
2024-04-16 15:18:46 -0300

    refactor: fixed the incorrect variable in createLayersIfNotExists

--- src/createlayers.js
+++ src/createlayers.js
@@ -5,7 +5,7 @@ export async function createLayersIfNotExists({ mainPath, defaultMainFolder, lay
     const defaultPath = `${mainPath}/${defaultMainFolder}`
     const folderToCreate = layers.filter(layer => !fs.existsSync(layer))
     const results = folderToCreate
-        .map(folder => fsPromises.mkdir(`${defaultPath}/${defaultMainFolder}`, { recursive: true }))
+        .map(folder => fsPromises.mkdir(`${defaultPath}/${folder}`, { recursive: true }))
 
     return Promise.all(results)
 }
