c17a55645cf75815c90152b4cbc640b1ac814a0a
RXGabriel <gabriel2005rf@gmail.com>
2024-04-17 10:26:37 -0300

    refactor: Fixed the import path in the factory template file, replacing 'service' with 'repository'

--- src/templates/factoryTemplate.js
+++ src/templates/factoryTemplate.js
@@ -8,7 +8,7 @@ const componentNameAnchor = "$$componentName"
 
 const template = `
 import $$serviceName from '../service/$$serviceNameDep.js'
-import $$repositoryName from '../service/$$repositoryNameDep.js'
+import $$repositoryName from '../repository/$$repositoryNameDep.js'
 
 export default class $$componentNameFactory {
     static getInstance() {
